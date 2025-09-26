198359de16b06a836d4fc34b52ac1659609002b5
Berk Şengül <berksengul17@gmail.com>
2025-09-26 12:39:13 +0300

    change absolute move with relative move

--- mouse_controller.py
+++ mouse_controller.py
@@ -4,11 +4,14 @@ import math
 
 SCREEN_WIDTH = 2560
 SCREEN_HEIGHT = 1440
-SMOOTHING = 0.4
+PADDING = 20
+SENSITIVITY = 4000
 
 class MouseController:
     def __init__(self):
-        self.prev_pos = (0, 0)
+        self.prev_index = None
+        self.prev_pos = None
+
         self.is_touching = False
         self.touch_start_time = 0.0
 
@@ -16,6 +19,8 @@ class MouseController:
         # ydotool expects integers
         x = int(x)
         y = int(y)
+
+        print(f"Move mouse {x}, {y}")
 
         try:
             subprocess.run(
@@ -34,17 +39,20 @@ class MouseController:
                     index = detection_result.hand_landmarks[i][8]
                     thumb = detection_result.hand_landmarks[i][4]
 
-                    target_x = (1 - index.x) * SCREEN_WIDTH
-                    target_y = index.y * SCREEN_HEIGHT
+                    # First frame: just initialize
+                    if self.prev_index is None:
+                        self.prev_index = (index.x, index.y)
+                        self.prev_pos = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+                        return
+                    
+                    dx = (index.x - self.prev_index[0]) * SENSITIVITY
+                    dy = (index.y - self.prev_index[1]) * SENSITIVITY
 
-                    prev_x, prev_y = self.prev_pos
+                    prev_x = self.prev_pos[0]
+                    prev_y = self.prev_pos[1]
 
-                    dx = target_x - prev_x
-                    dy = target_y - prev_y
-
-                    # Apply smoothing
-                    smooth_x = prev_x + SMOOTHING * dx
-                    smooth_y = prev_y + SMOOTHING * dy
+                    cur_x = max(PADDING, min(SCREEN_WIDTH - PADDING, prev_x - dx))  # note: minus to invert direction
+                    cur_y = max(PADDING, min(SCREEN_HEIGHT - PADDING, prev_y + dy))
 
                     # if touch detected, start a timer for hold
                     if self.detect_touch(index, thumb):
@@ -59,8 +67,7 @@ class MouseController:
                         # start moving the mouse around
                         hold_duration = now - self.touch_start_time
                         if (hold_duration > 1.0):
-                            self.smooth_move(prev_x, prev_y, smooth_x, smooth_y)
-                            self.prev_pos = (smooth_x, smooth_y)
+                            self.smooth_move(prev_x, prev_y, cur_x, cur_y)
        
                     else:
                         # Touch just ended
@@ -70,9 +77,10 @@ class MouseController:
 
                         # Interpolate small steps
                         if (abs(dx) >= 10 or abs(dy) >= 10):
-                            self.smooth_move(prev_x, prev_y, smooth_x, smooth_y)
-                            self.prev_pos = (smooth_x, smooth_y)
-
+                            self.smooth_move(prev_x, prev_y, cur_x, cur_y)
+                    
+                    self.prev_pos = (cur_x, cur_y)
+                    self.prev_index = (index.x, index.y)
 
     def smooth_move(self, prev_x, prev_y, target_x, target_y, steps=10, delay=0.005):
         for i in range(1, steps + 1):
